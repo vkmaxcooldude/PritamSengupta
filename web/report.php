9e4e7a492d9479064d3f975a8c947ab597efffd9
Vishal Kumar <vkmaxcooldude@gmail.com>
2018-03-24 00:02:10 +0530

    Check for asynchronus redirection AJAX

--- web/report.php
+++ web/report.php
@@ -1,9 +1,11 @@
-<html>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
   <title>Your Message has been received</title>
 </head>
 <body>
-  <h2><p>You have successfully sent your message. We will contact you as soon as possible.</p><p>Thank You for contacting us.</p></h2>
+  <h2><p>You have successfully sent your message.</p><p>We will contact you as soon as possible.</p><p>Thank You for contacting us.</p></h2>
 
   <?php
   $first_name = $_POST['firstname'];
@@ -12,14 +14,16 @@
   $body = $_POST['body'];
   $to = 'vishalk.engg@gmail.com';
   $subject = $_POST['subject'];
+  $header = 'From:' . "$email" . "\r\n" . 'Reply-To:' . "$email" . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
   $msg = "$first_name $last_name has sent you an E-mail\n" .
   "From: $email\n" .
   "Subject: $subject\n" .
-  'Message: \n\n' .
+  "Message: \n\n" .
   "$body\n";
-  mail($to, $subject, $msg, 'From:' . $email);
-  echo 'Thanks for submitting the form' . '<br />';
-  echo "Your message has been sent $first_name" . '<br />';
+  mail($to, $subject, $msg, $header);
+  echo 'Thanks for submitting the form' . "<br />";
+  echo "Your message has been sent $first_name" . "<br />";
   echo 'We will revert back to you as soon as possible' . '<br />';
   ?>
 
